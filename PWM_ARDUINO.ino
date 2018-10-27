788d9d04478c39f67ff8e255866db0043bd3efb3
Abhijeet Bhattacharya <31439714+Abhijeet399@users.noreply.github.com>
2018-10-27 19:05:31 +0530

    Extended lesson for the plan .Covers PWM, its advantages over potentiometer

---
+++ PWM_ARDUINO.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i=0;i<256;i++){
+  int j=map(i,0,255,0,5);
+  analogWrite(8,i);
+  Serial.print("Value of i:");
+  Serial.print(i);
+  Serial.print("             Value of i in Volts : ");
+  Serial.println(j);
+}
+}
